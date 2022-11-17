6453066b4fa5befcaf21c68fbbf0cfd0adbbbe00
JJH <JJH@LAPTOP-L34L5CV7>
2022-11-17 09:14:33 +0900

    01_bs 정수추가(a+b를 함.)

--- gitTest/src/com/bs/git/gitTest.java
+++ gitTest/src/com/bs/git/gitTest.java
@@ -5,6 +5,10 @@ public class gitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello git!!");
+		
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
 
 	}
 
